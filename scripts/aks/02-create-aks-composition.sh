45e5577113cb887a710f2cce9b4ee5c8e5e6b248
Paolo Salvatori <paolos@microsoft.com>
2024-02-07 17:57:41 +0100

    changes

--- scripts/aks/02-create-aks-composition.sh
+++ scripts/aks/02-create-aks-composition.sh
@@ -4,6 +4,10 @@
 # https://crossplane.io/docs/v1.3/getting-started/install-configure.html#install-crossplane
 # https://docs.crossplane.io/latest/getting-started/provider-azure-part-2/
 # https://marketplace.upbound.io/providers?query=azure
+# https://github.com/upbound/configuration-azure-aks/tree/main
+# https://marketplace.upbound.io/providers/crossplane-contrib/provider-kubernetes/v0.11.4
+# https://marketplace.upbound.io/providers/crossplane-contrib/provider-helm/v0.16.0
+
 
 # Create the Composition for the AKS resource
 kubectl apply -f aks-composition.yaml
